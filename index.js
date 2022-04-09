bf3f1140c1ec803ed03882944aca5bee28dbe914
jeehwan <tjtojan@naver.com>
2022-04-10 02:56:43 +0900

    permanent cookie

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ http.createServer(function(request, response) {
     if(request.headers.cookie !== undefined) {
         var cookies = cookie.parse(request.headers.cookie);
     }
-    
+
     console.log(cookies);
 
+    /*
     response.writeHead(200, {
-        'Set-Cookie' : ['yummy_cokkie=choco', 'tasty_cookie=strawberry']
-    });
+        'Set-Cookie' : [
+            'yummy_cokkie=choco', 
+            'tasty_cookie=strawberry',
+        `Permanent=cookies; Max-Age=${60*60*24*30}`]
+    }); */
     response.end("cookie!");
 }).listen(3001);
